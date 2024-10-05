04aebde38ce4b86e3ff95f25f28a7ac1c3a967d5
Emyr298 <emi.ru298@gmail.com>
2024-10-05 17:26:16 +0700

    feat: add content queries

--- src/main/java/com/cshare/content/repositories/ContentRepository.java
+++ src/main/java/com/cshare/content/repositories/ContentRepository.java
@@ -1,16 +1,38 @@
 package com.cshare.content.repositories;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 
 import com.cshare.content.models.Content;
+import com.cshare.content.models.ContentStatus;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
-public interface ContentRepository extends ReactiveCrudRepository<Content, UUID> {
+public interface ContentRepository extends ReactiveSortingRepository<Content, UUID> {
     Mono<Content> findById(UUID id);
+
     Flux<Content> findByUserId(UUID userId);
+
+    @Query("""
+            SELECT * FROM content
+            WHERE user_id = :userId
+                    AND status = :status
+                    AND (published_at BETWEEN :from AND :to)
+            LIMIT :limit
+        """)
+    Flux<Content> findByUserIdAndStatusAndPublishedAtBetweenLimit(
+        String userId,
+        ContentStatus status,
+        LocalDateTime from,
+        LocalDateTime to,
+        Integer limit
+    );
+
+    Flux<Content> findByUserIdAndStatusOrderByCreatedAt(String userId, ContentStatus status, Pageable pageable);
 }
